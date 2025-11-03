433b56d22919eda399e9357ab248a594056de092
IvansonJeffrey <ivanson.jeffrey@gmail.com>
2025-11-04 00:20:13 +0800

    Scene 3 Finish

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    public GameObject winPanel;
+
+    private int enemiesAlive = 0;
+
+    void Awake()
     {
-        
+        // simple singleton
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegisterEnemy()
     {
-        
+        enemiesAlive++;
+        Debug.Log("Enemy spawned. Alive: " + enemiesAlive);
+    }
+
+    public void UnregisterEnemy()
+    {
+        enemiesAlive--;
+        Debug.Log("Enemy killed. Alive: " + enemiesAlive);
+
+        if (enemiesAlive <= 0)
+        {
+            OnAllEnemiesDead();
+        }
+    }
+
+    void OnAllEnemiesDead()
+    {
+        Debug.Log("YOU WIN!");
+
+        if (winPanel != null)
+        {
+            winPanel.SetActive(true);
+        }
+
+        Time.timeScale = 0f;
     }
 }
--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,21 +7,70 @@ public class Navigation : MonoBehaviour
     public Transform player;
     private Animator animator;
 
+    [Header("Explosion Settings")]
+    public GameObject explosionPrefab;   // same prefab as Bullet.explosionPrefab
+    public AudioClip explosionSound;     // same clip as Bullet.explosionSound
+    public float explodeDistance = 1.5f; // how close to player before exploding
+    public float killDelay = 0.0f;       // delay before destroying objects (optional)
+
+    private bool hasExploded = false;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (player != null)
-            agent.SetDestination(player.position);
+        if (player == null || hasExploded) return;
 
+        // Chase the player
+        agent.SetDestination(player.position);
+
+        // Update run/walk animation
         if (animator != null)
         {
-            // Set Speed based on agent’s velocity
             animator.SetFloat("Speed", agent.velocity.magnitude);
         }
+
+        // Check distance to player
+        float dist = Vector3.Distance(transform.position, player.position);
+        if (dist <= explodeDistance)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        if (hasExploded) return;
+        hasExploded = true;
+
+        // Stop moving
+        if (agent != null) agent.isStopped = true;
+
+        Vector3 pos = transform.position + Vector3.up * 0.1f;
+
+        // Spawn explosion particle
+        if (explosionPrefab != null)
+        {
+            Instantiate(explosionPrefab, pos, Quaternion.identity);
+        }
+
+        // Play explosion sound
+        if (explosionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, pos);
+        }
+
+        // Kill the player (simple version: destroy the GameObject)
+        if (player != null)
+        {
+            Destroy(player.gameObject, killDelay);
+        }
+
+        // Kill this enemy
+        Destroy(gameObject, killDelay);
     }
 }
